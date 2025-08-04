51b3e59ac3bbaeaeeb31e091da291c04311ac458
Naseem Ali <c.naseem01@gmail.com>
2025-08-04 18:04:07 +0100

    scripts

--- app/prov-db.sh
+++ app/prov-db.sh
@@ -2,16 +2,16 @@
 
 #Scipt for the database
 echo "update..."
-sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
 echo "update done"
 echo
  
 echo "upgrade..."
-sudo apt upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 echo "upgrade done"
 echo
  
-sudo apt-get install gnupg curl
+sudo DEBIAN_FRONTEND=noninteractive apt-get install gnupg curl
  
 echo "import public key"
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
@@ -23,10 +23,10 @@ echo
 #create list file
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
  
-sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
  
 #install mongodb
-sudo apt-get install -y \
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
    mongodb-org=7.0.22 \
    mongodb-org-database=7.0.22 \
    mongodb-org-server=7.0.22 \
@@ -35,3 +35,26 @@ sudo apt-get install -y \
    mongodb-org-mongos=7.0.22 \
    mongodb-org-tools=7.0.22 \
    mongodb-org-database-tools-extra=7.0.22
+
+   echo 
+echo " Configuring MongoDB to Allow External Connections"
+echo 
+ 
+# Backup existing config
+sudo cp /etc/mongod.conf /etc/mongod.conf.bk
+ 
+# Update bindIp to 0.0.0.0
+sudo sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
+echo "bindIp updated to 0.0.0.0"
+echo
+ 
+echo 
+echo " Starting MongoDB..."
+echo 
+sudo systemctl start mongod
+sudo systemctl enable mongod
+ 
+echo
+echo 
+echo " MongoDB provisioned and running "
+echo 
